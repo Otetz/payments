19d5b938fe95081990525bfcac8242bad4527380
Alexey Shevchenko <otetz@me.com>
2019-07-22 02:07:38 +0300

    CLI flags

--- README.md
+++ README.md
@@ -3,6 +3,25 @@ Payments
 
 ## Project purpose
 
+Payment system, provides ability to transfer money between accounts. 
+
+System provide reports: 
+ - all registered accounts; 
+ - all registered payments (transfers).
+
+## Usage
+
+### Command-line flags
+ - `-app_name` _string_ -- PostgreSQL application name (for logging) (default "payments")
+ - `-database` _string_ -- PostgreSQL database name (default "payments")
+ - `-db_address` _string_ -- Address to connect to PostgreSQL server (default "localhost:5432")
+ - `-db_log` -- Switch for statements logging
+ - `-db_password` _string_ -- PostgreSQL connection password
+ - `-db_user` _string_ -- PostgreSQL connection user (default "postgres")
+ - `-http_address` _string_ -- Http address for web server running (default "localhost:8080")
+ - `-pool_size` _int_ -- PostgreSQL connection pool size (default 10)
+
+
 ## Dependencies
 
 - [go-kit](http://github.com/go-kit/kit) -- toolkit for building microservices, recommended by design;
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -27,36 +28,105 @@ func (d dbLogger) AfterQuery(q *pg.QueryEvent) {
 	fmt.Println(q.FormattedQuery())
 }
 
+var (
+	flagHttpAddr = flag.String("http_address", "localhost:8080", "Http address for web server running")
+
+	flagDBAddr     = flag.String("db_address", "localhost:5432", "Address to connect to PostgreSQL server")
+	flagDBUser     = flag.String("db_user", "postgres", "PostgreSQL connection user")
+	flagDBPassword = flag.String("db_password", "", "PostgreSQL connection password")
+	flagDBDatabase = flag.String("database", "payments", "PostgreSQL database name")
+	flagDBAppName  = flag.String("app_name", "payments", "PostgreSQL application name (for logging)")
+	flagDBPoolSize = flag.Int("pool_size", 10, "PostgreSQL connection pool size")
+	flagDBLog = flag.Bool("db_log", false, "Switch for statements logging")
+)
+
 func main() {
-	httpAddr := ":8095"
+	flag.Parse()
 
 	logger := log.NewLogfmtLogger(os.Stderr)
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
 
-	conn := pg.Connect(&pg.Options{
-		Addr:            "localhost:5432", // TODO: enviflag
-		User:            "postgres",       // TODO: enviflag
-		Password:        "example",        // TODO: enviflag
-		Database:        "payments",       // TODO: enviflag
-		ApplicationName: "payments",       // TODO: enviflag
-		PoolSize:        10,               // TODO: enviflag
-	})
-	defer conn.Close()
-	conn.AddQueryHook(dbLogger{})
+	conn := setupDB(logger)
+	defer func() {
+		if err := conn.Close(); err != nil {
+			_ = logger.Log("error", err)
+		}
+	}()
 
-	if err := db.CreateSchema(conn); err != nil {
-		_ = logger.Log("transport", "DB", "address", "172.18.0.2", "msg", err)
-	}
-
-	//var (
-	//	accounts = inmem.NewAccountRepository()
-	//	payments = inmem.NewPaymentRepository(accounts)
-	//)
 	var (
 		accounts = db.NewAccountRepository(conn)
 		payments = db.NewPaymentRepository(conn, accounts)
 	)
 
+	as := setupAccountService(accounts, logger)
+	ps := setupPaymentService(payments, accounts, logger)
+
+	httpLogger := log.With(logger, "component", "http")
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/api/accounts/v1/", account.MakeHandler(as, httpLogger))
+	mux.Handle("/api/payments/v1/", payment.MakeHandler(ps, httpLogger))
+
+	http.Handle("/", accessControl(mux))
+	http.Handle("/metrics", promhttp.Handler())
+
+	errs := make(chan error, 2)
+	go func() {
+		_ = logger.Log("transport", "http", "address", *flagHttpAddr, "msg", "listening")
+		errs <- http.ListenAndServe(*flagHttpAddr, nil)
+	}()
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	_ = logger.Log("terminated", <-errs)
+}
+
+func setupDB(logger log.Logger) *pg.DB {
+	conn := pg.Connect(&pg.Options{
+		Addr:            *flagDBAddr,
+		User:            *flagDBUser,
+		Password:        *flagDBPassword,
+		Database:        *flagDBDatabase,
+		ApplicationName: *flagDBAppName,
+		PoolSize:        *flagDBPoolSize,
+	})
+	if *flagDBLog {
+		conn.AddQueryHook(dbLogger{})
+	}
+	if err := db.CreateSchema(conn); err != nil {
+		_ = logger.Log("transport", "DB", "address", *flagDBAddr, "msg", err)
+	}
+	return conn
+}
+
+func setupPaymentService(payments payment.Repository, accounts account.Repository, logger log.Logger) payment.Service {
+	fieldKeys := []string{"method"}
+
+	ps := payment.NewService(payments, accounts)
+	ps = payment.NewLoggingService(log.With(logger, "component", "payment"), ps)
+	ps = payment.NewMetricsService(
+		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: "payment_service",
+			Name:      "request_count",
+			Help:      "Number of requests received.",
+		}, fieldKeys),
+		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "api",
+			Subsystem: "payment_service",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, fieldKeys),
+		ps,
+	)
+	return ps
+}
+
+func setupAccountService(accounts account.Repository, logger log.Logger) account.Service {
 	fieldKeys := []string{"method"}
 
 	as := account.NewService(accounts)
@@ -76,53 +146,13 @@ func main() {
 		}, fieldKeys),
 		as,
 	)
-
-	ps := payment.NewService(payments, accounts)
-	ps = payment.NewLoggingService(log.With(logger, "component", "payment"), ps)
-	ps = payment.NewMetricsService(
-		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-			Namespace: "api",
-			Subsystem: "paymemt_service",
-			Name:      "request_count",
-			Help:      "Number of requests received.",
-		}, fieldKeys),
-		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-			Namespace: "api",
-			Subsystem: "paymemt_service",
-			Name:      "request_latency_microseconds",
-			Help:      "Total duration of requests in microseconds.",
-		}, fieldKeys),
-		ps,
-	)
-
-	httpLogger := log.With(logger, "component", "http")
-
-	mux := http.NewServeMux()
-
-	mux.Handle("/api/accounts/v1/", account.MakeHandler(as, httpLogger))
-	mux.Handle("/api/payments/v1/", payment.MakeHandler(ps, httpLogger))
-
-	http.Handle("/", accessControl(mux))
-	http.Handle("/metrics", promhttp.Handler())
-
-	errs := make(chan error, 2)
-	go func() {
-		_ = logger.Log("transport", "http", "address", httpAddr, "msg", "listening")
-		errs <- http.ListenAndServe(httpAddr, nil)
-	}()
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT)
-		errs <- fmt.Errorf("%s", <-c)
-	}()
-
-	_ = logger.Log("terminated", <-errs)
+	return as
 }
 
 func accessControl(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 
 		if r.Method == "OPTIONS" {
